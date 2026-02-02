8d9c4bc367a2aff5ca6987ad265806ae4fcb49ec
Heet Sankesara <heetsankesara3@gmail.com>
2026-02-02 11:39:55 +0000

    Added data simulation module

---
+++ airflow-dynamic-config/dagloader/conditionparser.py
@@ -0,0 +1,24 @@
+import rule_engine as re
+from airflow.sdk import task
+
+class ConditionParser:
+    def __init__(self, condition_str: str, condition_name: str = "default_condition"):
+        self.condition_str = condition_str
+        self.condition_name = condition_name
+        self.engine = re.Rule(condition_str)
+
+    def evaluate(self, context: dict) -> bool:
+        return self.engine.matches(context)
+
+    def get_conditional_task(self, task, true_task, context: dict):
+        @task.branch(task_id=self.condition_name + "_branch_task")
+        @staticmethod
+        def _airflow_task_condition(task) -> bool:
+            """Example method to evaluate conditions based on Airflow task context."""
+            # This is a placeholder for actual condition logic based on Airflow context
+            return self.evaluate(context)
+
+        if self.evaluate(context):
+            return _airflow_task_condition(task)
+        else:
+            return None
