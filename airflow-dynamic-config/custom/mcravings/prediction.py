197103017bfbbe103a52b737f3ccd3668716088c
Heet Sankesara <heetsankesara3@gmail.com>
2026-09-02 15:17:30 +0530

    changed prediction threshold to 45 mins

--- airflow-dynamic-config/custom/mcravings/prediction.py
+++ airflow-dynamic-config/custom/mcravings/prediction.py
@@ -32,11 +32,10 @@ class PredictionTaskProcessor():
         with open(self.MODEL_DIR / 'model_pipeline.pickle', 'rb') as f :
             loaded_model = pickle.load(f)
         df['prediction_prob'] = loaded_model.predict_proba(df[loaded_model.feature_names_in_])[:, 1]
-        logger.info(f"df.values: {df.values}")
         df['prediction'] = (df['prediction_prob'] > 0.5).astype(int)
         # if multiple true predictions exist for a participant, keep the latest window's prediction only
         prediction_df = df.loc[df['prediction'] == 1]
         prediction_df = prediction_df.loc[prediction_df.groupby('participant_id')['window_start'].idxmax()]
         logger.info(f"Predicted craving for participants {prediction_df['participant_id'].values} for timestamps {prediction_df['window_start'].values}")
-        prediction_df = prediction_df.loc[prediction_df['window_start'] > pd.Timestamp.now(tz='UTC') - pd.Timedelta(minutes=30)]
+        prediction_df = prediction_df.loc[prediction_df['window_start'] > pd.Timestamp.now(tz='UTC') - pd.Timedelta(minutes=45)]
         return prediction_df[['participant_id', 'prediction', 'window_start']].to_dict(orient='records')
